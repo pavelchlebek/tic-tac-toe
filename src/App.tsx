680da0d70e0b1230bcca07db3f340cdf729c1961
pavelchlebek <pavelchlebek990@gmail.com>
2022-02-26 11:39:37 +0100

    my first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Palko je bůh!!! And he is working with TS so well!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
